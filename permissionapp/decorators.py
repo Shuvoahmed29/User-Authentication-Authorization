ff6da5d4de98afd850d4651ee806fecfb6e9e071
Md.Raihanul Haque(Shuvo) <67226278+Shuvoahmed29@users.noreply.github.com>
2025-04-08 08:28:48 +0600

    8/4/25

---
+++ permissionapp/decorators.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.decorators import user_passes_test
+
+def group_required(*group_names):
+    def check_group(user):
+        if user.is_superuser:
+            return True
+
+        for group in group_names:
+            if user.groups.filter(name=group).exists():
+                return True
+    
+    return user_passes_test(check_group,login_url='permission/home/')
+
+admin_required = group_required('admin')
+editor_required = group_required('editor','admin')
+author_required = group_required('author','editor','admin')
--- permissionapp/views.py
+++ permissionapp/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth.decorators import login_required
+from . decorators import admin_required,editor_required,author_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-@login_required
+# @login_required
+# def admin(request):
+#     user = request.user
+#     if user.is_superuser:
+#         return render(request, 'admin.html')
+#     else:
+#         return HttpResponse("Unauthorized access")
+
+# @login_required
+# def editor(request):
+#     user = request.user
+#     if user.is_superuser or user.groups.filter(name="editor").exists():
+#         return render(request, 'editor.html')
+#     else:
+#         return HttpResponse("Editor Unauthorized access")
+
+# @login_required
+# def author(request):
+#     user = request.user
+#     if user.is_superuser or user.groups.filter(name="author").exists() or user.groups.filter(name="editor").exists():
+#         return render(request, 'author.html')
+#     else:
+#         return HttpResponse("Author Unauthorized access")
+
+
+@admin_required
 def admin(request):
-    user = request.user
-    if user.is_superuser:
-        return render(request, 'admin.html')
-    else:
-        return HttpResponse("Unauthorized access")
+    return render(request, 'admin.html')
 
-@login_required
+@editor_required
 def editor(request):
-    user = request.user
-    if user.groups.filter(name="editor").exists():
-        return render(request, 'editor.html')
-    else:
-        return HttpResponse("Editor Unauthorized access")
+    return render(request, 'editor.html')
 
-@login_required
+@author_required
 def author(request):
-    user = request.user
-    if user.groups.filter(name="author").exists():
-        return render(request, 'author.html')
-    else:
-        return HttpResponse("Author Unauthorized access")
+    return render(request, 'author.html')
